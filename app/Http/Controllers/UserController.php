d4a5ffabbfec8ed313d1d5eb5653b26f973042d2
ndvelasquez <nestorsox35@gmail.com>
2021-04-21 11:34:36 -0500

    añadida autenticacion del formulario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade as PDF;
 use DateTime;
 
 class UserController extends Controller
 {
-    public function generatePdf(Request $request)
+    public function generatePdf(UserRequest $request)
     {
-        $user = $request->all();
-        // dd($request->age);
+        $user = $request->validated();
         $birthAge = new DateTime($request->age);
         $today = new DateTime();
         $age = $today->diff($birthAge);
         $user['age'] = $age->y;
-        // dd($user);
         if ($request->testType == 'Antigeno') {
             $pdf = PDF::loadView('reports.antigen', compact('user'));
         }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,16 @@
     <body class="p-4 max-w-7xl mx-auto sm:px-6 lg:px-8 bg-gray-200">
         <h1 class="text-lg font-bold">Generar reporte de resultados de la prueba de COVID-19</h1>
 
+        @if ($errors->any())
+                <ul class="list-none bg-red-100 text-red-500 p-4 mb-4">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
         <div class="grid md:grid-cols-6 gap-4">
-            <form action="{{ route('users.pdf') }}" method="POST">
+            <form id="myForm" action="{{ route('users.pdf') }}" method="POST">
                 @csrf
                 <p class="block font-medium text-sm text-gray-700">Tipo de prueba:</p>
                 <input type="radio" id="rapid" name="testType" value="Serologica">
@@ -24,20 +32,20 @@
                 <input type="checkbox" onchange="ActiveTravelInput(this)" class="form-input rounded-md" name="traveling" id="traveling" value="travel">
                 <label for="traveling" class="text-sm font-medium text-gray-700">Muestra para viaje</label>
                 <label class="block font-medium text-sm text-gray-700" for="dni">Documento de identidad:</label>
-                <input class="form-input w-full rounded-md shadow-sm" type="text" name="dni">
+                <input class="form-input w-full rounded-md shadow-sm" type="text" name="dni" required>
                 <label class="block font-medium text-sm text-gray-700" id="passportLabel" style="display: none" for="passport">Pasaporte:</label>
                 <input class="form-input w-full rounded-md shadow-sm" id="passportInput" style="display: none" type="text" name="passport">
                 <label class="block font-medium text-sm text-gray-700" for="name">Nombre(s):</label>
-                <input class="form-input w-full rounded-md shadow-sm" type="text" name="name">
+                <input class="form-input w-full rounded-md shadow-sm" type="text" name="name" required>
                 <label class="block font-medium text-sm text-gray-700" for="lastname">Apellido(s):</label>
-                <input class="form-input w-full rounded-md shadow-sm" type="text" name="lastname">
+                <input class="form-input w-full rounded-md shadow-sm" type="text" name="lastname" required>
                 <p class="block font-medium text-sm text-gray-700">Sexo:</p>
                 <input type="radio" id="male" name="gender" value="Masculino">
                 <label for="male">Masculino</label>
                 <input type="radio" id="female" name="gender" value="Femenino">
                 <label for="female">Femenino</label>
                 <label class="block font-medium text-sm text-gray-700" for="age">Fec. Nacimiento:</label>
-                <input class="form-input w-full rounded-md shadow-sm" type="date" name="age">
+                <input class="form-input w-full rounded-md shadow-sm" type="date" name="age" required>
                 <p class="block font-medium text-sm text-gray-700">Referencia:</p>
                 <input type="radio" id="home" name="reference" value="Domicilio">
                 <label for="home">Domicilio</label>
@@ -49,7 +57,7 @@
                 <input type="radio" id="positive" name="result" value="Positivo">
                 <label for="positive">Positivo</label>
     
-                <input type="submit" class="border-2 cursor-pointer text-white bg-green-500 hover:bg-green-700 rounded-md my-2 py-2 px-4" value="Generar">
+                <input type="submit" class="border-2 cursor-pointer text-white bg-green-500 hover:bg-green-700 rounded-md my-2 py-2 px-4" value="Generar" onclick="setTimeout(resetForm, 50000)">
             </form>
         </div>
     </body>
@@ -65,6 +73,10 @@
                 document.getElementById('passportLabel').style.display='none';
                 document.getElementById('passportInput').style.display='none';
             }
+        }
+        function resetForm()
+        {
+            document.getElementById('myForm').reset();
         }
     </script>
 </html>
