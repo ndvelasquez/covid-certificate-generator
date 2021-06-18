9abaa89d03e8af88b366d45a2b8ce52b25a43957
ndvelasquez <nestorsox35@gmail.com>
2021-06-18 09:32:44 -0500

    optimizacion de reporte de prueba serologica

--- resources/views/reports/serologic.blade.php
+++ resources/views/reports/serologic.blade.php
@@ -3,58 +3,70 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <style>
             * {
                 margin: 0;
                 padding: 0;
+            }
+            body {
+                font-family: Verdana, Geneva, Tahoma, sans-serif;
+            }
+            p {
+                font-size: 12px;
+            }
+            table, td, th {
+                font-size: 14px;
+            }
+
+            .data td {
+                padding: 2px 10px;
             }
         </style>
 
         <title>reporte de toma de prueba covid-19</title>
 
     </head>
-    <body class="bg-cover" style="background-image: url({{ public_path('images/template.png') }})">
-        <div class="px-8 mt-20">
-            <img class="w-48 h-16 mb-6" src="{{ public_path('images/minsa.png') }}" alt="">
+    <body style="background-image: url({{ public_path('images/template.png') }})">
+        <div style="margin-top: 40px; padding: 60px;">
+            <img height="60" width="150" style="margin-bottom: 30px;" src="{{ public_path('images/minsa.png') }}" alt="">
             <p class="text-sm text-left">Autorizado por: Ministerio de salud del Perú</p>
             <p class="text-xs text-left">Ipress: 0026511</p>
-            <table class="table-fixed w-full text-sm my-2">
+            <table style="margin: 10px 0px; width: 100%">
                 <tr>
-                    <td>PACIENTE: {{ $user['name'] }} {{ $user['lastname'] }}</td>
-                    <td class="text-right"><strong>FECHA:</strong> {{ date("d/m/Y") }}</td>
+                    <td>PACIENTE: {{ $test->client->FullName }}</td>
+                    <td style="text-align: right;"><strong>FECHA:</strong> {{ $test->DateOfTest }}</td>
                 </tr>
                 <tr>
                     <td>
-                        DNI/CE/OTRO: {{ $user['dni'] }}
-                        @if ($user['passport'] != null)
+                        DNI/CE/OTRO: {{ $test->client->document_number }}
+                        @if ($test->client->passport != null)
                             <br>PASAPORTE:<br>
-                            {{ $user['passport'] }}
+                            {{ $test->client->passport }}
                         @endif
                     </td>
-                    <td class="text-right"><strong>SEXO:</strong> {{ $user['gender'] }}</td>
+                    <td style="text-align: right;"><strong>SEXO:</strong> {{ $test->client->gender }}</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td class="text-right"><strong>EDAD:</strong> {{ $user['age'] }}</td>
+                    <td style="text-align: right;"><strong>EDAD:</strong> {{ $test->client->age }}</td>
                 </tr>
             </table>
 
-            <p class="text-center text-md font-bold">INMUNOBIOQUIMICA</p>
-            <p class="text-center text-md font-bold">DETECCION DE ANTICUERPOS IGM / IGG DEL SARS COV 2</p>
+            <p style="text-align: center; font-size: 16px"><strong>INMUNOBIOQUIMICA</strong></p>
+            <p style="text-align: center; font-size: 16px"><strong>DETECCION DE ANTICUERPOS IGM / IGG DEL SARS COV 2</strong></p>
 
-            <table class="table-fixed w-full my-4">
+            <table class="data" style="margin-top: 30px; border-collapse: collapse; width: 100%">
                 <tr>
-                    <td class="border text-sm text-center font-bold" style="border-color: black">
+                    <td style="border: 1px solid black; font-weight: bold; text-align: center;">
                         Examen
                     </td>
-                    <td class="border text-sm text-center font-bold" style="border-color: black">
+                    <td style="border: 1px solid black; font-weight: bold; text-align: center;">
                         Resultado
                     </td>
                 </tr>
                 <tr>
-                    <td class="text-sm font-bold">SARS COV 2, ANTICUERPOS-IGM (COVID 19) Inmunocromatografía</td>
-                    <td class="text-md font-bold text-center uppercase" style="color: red">{{ $user['result'] }}</td>
+                    <td style="font-weight: bold;">SARS COV 2, ANTICUERPOS-IGM (COVID 19) Inmunocromatografía</td>
+                    <td style="color: red; font-weight: bold; text-align: center; text-transform: uppercase;">{{ $test->test_result }}</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
@@ -65,45 +77,45 @@
                     <td>&nbsp;</td>
                 </tr>
                 <tr>
-                    <td class="text-sm font-bold">SARS COV 2, ANTICUERPOS-IGG (COVID 19) Inmunocromatografía</td>
-                    <td class="text-md font-bold text-center uppercase" style="color: red">{{ $user['result'] }}</td>
+                    <td style="font-weight: bold;">SARS COV 2, ANTICUERPOS-IGG (COVID 19) Inmunocromatografía</td>
+                    <td style="color: red; font-weight: bold; text-align: center; text-transform: uppercase;">{{ $test->test_result }}</td>
                 </tr>
             </table>
-            <table class="table-fixed w-full my-8">
+            <table class="data" style="border-collapse: collapse; width:100%; margin-top: 30px;">
                 <tr>
-                    <td colspan="3" class="text-md font-bold text-center" style="color: red">CUADRO PARA INTERPRETACION POR EL MEDICO TRATANTE</td>
+                    <td colspan="3" style="color: red; font-weight: bold; text-align: center">CUADRO PARA INTERPRETACION POR EL MEDICO TRATANTE</td>
                 </tr>
                 <tr>
-                    <td colspan="2" class="border text-sm font-bold text-center" style="border-color: black">Resultado de anticuerpos</td>
-                    <td rowspan="2" class="border text-center text-sm font-bold" style="border-color: black">Significado clínico</td>
+                    <td colspan="2" style="border: 1px solid black; font-weight: bold; text-align: center;">Resultado de anticuerpos</td>
+                    <td rowspan="2" style="border: 1px solid black; font-weight: bold; text-align: center;">Significado clínico</td>
                 </tr>
                 <tr>
-                    <td class="border text-sm text-center font-bold" style="border-color: black; color: red">IgM</td>
-                    <td class="border text-sm text-center font-bold" style="border-color: black; color: red">IgG</td>
+                    <td style="border: 1px solid black; font-weight: bold; text-align: center; color: red;">IgM</td>
+                    <td style="border: 1px solid black; font-weight: bold; text-align: center; color: red;">IgG</td>
                 </tr>
                 <tr>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">negativo</td>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">negativo</td>
-                    <td class="border text-sm" style="border-color: black">Paciente puede estar en periodo de ventana o no ha sido expuesto</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">negativo</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">negativo</td>
+                    <td style="border: 1px solid black;">Paciente puede estar en periodo de ventana o no ha sido expuesto</td>
                 </tr>
                 <tr>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">positivo</td>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">negativo</td>
-                    <td class="border text-sm" style="border-color: black">Paciente puede estar en la etapa inicial de la infección</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">positivo</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">negativo</td>
+                    <td style="border: 1px solid black;">Paciente puede estar en la etapa inicial de la infección</td>
                 </tr>
                 <tr>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">positivo</td>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">positivo</td>
-                    <td class="border text-sm" style="border-color: black">Paciente está en la etapa activa o en recuperación de la infección</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">positivo</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">positivo</td>
+                    <td style="border: 1px solid black;">Paciente está en la etapa activa o en recuperación de la infección</td>
                 </tr>
                 <tr>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">negativo</td>
-                    <td class="border text-center text-sm font-bold uppercase" style="border-color: black">positivo</td>
-                    <td class="border text-sm" style="border-color: black">Paciente puede estar en la etapa tardía o puede haber tenido una infección pasada y se ha recuperado</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">negativo</td>
+                    <td style="border: 1px solid black; text-align: center; font-weight: bold; text-transform: uppercase">positivo</td>
+                    <td style="border: 1px solid black;">Paciente puede estar en la etapa tardía o puede haber tenido una infección pasada y se ha recuperado</td>
                 </tr>
             </table>
 
-            <p class="my-8">&nbsp;</p>
+            <p style="margin-top: 30px;">&nbsp;</p>
 
             <p><img src="{{ public_path('images/sign.png') }}" alt=""></p>
             <p class="text-xs">Dr. CARLOS ECHEVERRIA ESCRIBENS <br> C.M.P 27834 - R.N.E. 16271</p>
